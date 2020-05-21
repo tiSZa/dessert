7e9cc05c5c915fb6b332aa40bcc4decc6a116abb
tiSZa <61270735+tiSZa@users.noreply.github.com>
2020-05-21 16:50:47 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+function getSoups() {
+  document.getElementById("getDishes").src = "https://tisza.github.io/recipes/soups";
+}
+  function getGarnises() {
+  document.getElementById("getDishes").src = "https://tisza.github.io/recipes/garnishes";
+  }
+    function getDesserts() {
+  document.getElementById("getDishes").src = "https://tisza.github.io/recipes/desserts";
+    }
+      function getMainCourses() {
+  document.getElementById("getDishes").src = "https://tisza.github.io/recipes/mainCourses";
+}
+  function getPastas() {
+  document.getElementById("getDishes").src = "https://tisza.github.io/recipes/pastas";
+  }
