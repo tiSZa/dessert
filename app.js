82c56228093d2453dd4ba92fbc9be50d434af612
Tiszai András <tiszuss@gmail.com>
2020-07-08 18:50:07 +0200

    add base file structure

---
+++ app.js
@@ -0,0 +1,22 @@
+function getSoups() {
+  document.getElementById('getDishes').src = 'https://tisza.github.io/recipes/soups';
+}
+
+function getGarnises() {
+  document.getElementById('getDishes').src = 'https://tisza.github.io/recipes/garnishes';
+}
+
+function getDesserts() {
+  document.getElementById('getDishes').src = 'https://tisza.github.io/recipes/desserts';
+  }
+
+function getMainCourses() {
+  document.getElementById('getDishes').src = 'https://tisza.github.io/recipes/mainCourses';
+}
+
+function getPastas() {
+  document.getElementById('getDishes').src = 'https://tisza.github.io/recipes/pastas';
+  }
+function scrollWin() {
+ window.scrollTo(0, 300);
+}
