6e9189a2dc0a823bf25dabc19ebb3d7d27a482bb
Tiszai András <tiszuss@gmail.com>
2020-09-02 15:54:38 +0200

    repair v.1.9

--- app.js
+++ app.js
@@ -20,38 +20,38 @@ function getPastas() {
 
 
 
-function scrollWin() {  
+function scrollWin(){  
   let recipe = document.getElementById("menu");
   if (recipe.value == "1"){
     let elmnt1 = document.getElementById("recipeWrapper1");
     elmnt1.scrollIntoView(true);
   }
-  if (recipe.value == "2"){
+  else if (recipe.value == "2"){
     let elmnt2 = document.getElementById("recipeWrapper2");
     elmnt2.scrollIntoView(true);
   }
-  if (recipe.value == "3"){
+  else if (recipe.value == "3"){
     let elmnt3 = document.getElementById("recipeWrapper3");
     elmnt3.scrollIntoView(true);
   }
-  if (recipe.value == "4"){
-    let elmnt4 = document.getElementById("recipeWrapper4");  
+  else if (recipe.value == "4"){
+    let elmnt4 = document.getElementById("recipeWrapper4"); 
     elmnt4.scrollIntoView(true);
   }
-  if (recipe.value == "5"){
-    let elmnt5 = document.getElementById("recipeWrapper5");  
+  else if (recipe.value == "5"){
+    let elmnt5 = document.getElementById("recipeWrapper5"); 
     elmnt5.scrollIntoView(true);
   }
-  if (recipe.value == "6"){
-    let elmnt6 = document.getElementById("recipeWrapper6");  
+  else if (recipe.value == "6"){
+    let elmnt6 = document.getElementById("recipeWrapper6"); 
     elmnt6.scrollIntoView(true);
   }
-  if (recipe.value == "7"){
-    let elmnt7 = document.getElementById("recipeWrapper7");  
+  else if (recipe.value == "7"){
+    let elmnt7 = document.getElementById("recipeWrapper7"); 
     elmnt7.scrollIntoView(true);
   }
-  if (recipe.value == "8"){
-    let elmnt8 = document.getElementById("recipeWrapper8");  
+  else if (recipe.value == "8"){
+    let elmnt8 = document.getElementById("recipeWrapper8"); 
     elmnt8.scrollIntoView(true);
   }
 }
