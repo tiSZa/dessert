503498b7e56ecfe1ffc8bc51b362b82e33cb946c
Tiszai András <tiszuss@gmail.com>
2020-10-18 15:34:26 +0200

    repair v2.3

--- app.js
+++ app.js
@@ -1,23 +1,7 @@
-function getSoups() {
-  document.getElementById('getDishes').src = 'https://tisza.github.io/recipes/soups';
+function getDish(source) {
+  let dish = source
+  document.getElementById('getDishes').src = 'https://tisza.github.io/recipes/' + dish;
 }
-
-function getGarnises() {
-  document.getElementById('getDishes').src = 'https://tisza.github.io/recipes/garnishes';
-}
-
-function getDesserts() {
-  document.getElementById('getDishes').src = 'https://tisza.github.io/recipes/desserts';
-  }
-
-function getMainCourses() {
-  document.getElementById('getDishes').src = 'https://tisza.github.io/recipes/mainCourses';
-}
-
-function getPastas() {
-  document.getElementById('getDishes').src = 'https://tisza.github.io/recipes/pastas';
-  }
-
 
 
 function scrollWin(){  
