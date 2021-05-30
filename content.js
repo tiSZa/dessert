f94c0fa4e2fd88cda3163d014961f88383f785ed
Tiszai András <tiszuss@gmail.com>
2021-05-30 17:12:45 +0200

    repaired js file

--- content.js
+++ content.js
@@ -1,35 +1,35 @@
 function scrollWin(){  
   let recipe = document.getElementById("menu");
   if (recipe.value == "1"){
-    let elmnt1 = document.getElementById("recipeWrapper1");
+    let elmnt1 = document.getElementsByClassName("recipeWrapper1");
     elmnt1.scrollIntoView(true);
   }
   else if (recipe.value == "2"){
-    let elmnt2 = document.getElementById("recipeWrapper2");
+    let elmnt2 = document.getElementsByClassName("recipeWrapper2");
     elmnt2.scrollIntoView(true);
   }
   else if (recipe.value == "3"){
-    let elmnt3 = document.getElementById("recipeWrapper3");
+    let elmnt3 = document.getElementsByClassName("recipeWrapper3");
     elmnt3.scrollIntoView(true);
   }
   else if (recipe.value == "4"){
-    let elmnt4 = document.getElementById("recipeWrapper4"); 
+    let elmnt4 = document.getElementsByClassName("recipeWrapper4"); 
     elmnt4.scrollIntoView(true);
   }
   else if (recipe.value == "5"){
-    let elmnt5 = document.getElementById("recipeWrapper5"); 
+    let elmnt5 = document.getElementsByClassName("recipeWrapper5"); 
     elmnt5.scrollIntoView(true);
   }
   else if (recipe.value == "6"){
-    let elmnt6 = document.getElementById("recipeWrapper6"); 
+    let elmnt6 = document.getElementsByClassName("recipeWrapper6"); 
     elmnt6.scrollIntoView(true);
   }
   else if (recipe.value == "7"){
-    let elmnt7 = document.getElementById("recipeWrapper7"); 
+    let elmnt7 = document.getElementsByClassName("recipeWrapper7"); 
     elmnt7.scrollIntoView(true);
   }
   else if (recipe.value == "8"){
-    let elmnt8 = document.getElementById("recipeWrapper8"); 
+    let elmnt8 = document.getElementsByClassName("recipeWrapper8"); 
     elmnt8.scrollIntoView(true);
   }
 }
