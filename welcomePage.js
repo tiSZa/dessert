5f11cc7f55cfa4f7bb7c2118f827fc1765f6dcef
Tiszai András <tiszuss@gmail.com>
2022-01-02 14:23:26 +0100

    add welcome files

--- welcomePage.js
+++ welcomePage.js
@@ -1,3 +1,8 @@
 let date = new Date();
-let currentDate = date.getFullYear() + "." + date.getMonth() + "." + date.getDate() + ".";
-document.getElementById("date").innerHTML = currentDate;
+let year = date.getFullYear();
+let month = date.getMonth() + 1;
+let day = date.getDate();
+let currentDate = year + "." + month + "." + day + ".";
+function myFunction() {
+    document.getElementById("date").innerHTML = currentDate;
+}
